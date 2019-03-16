1bb1313e079e9aea819da92f658a09840d3e1b58
Coskun Caner <cskncnr@gmail.com>
2019-03-16 18:30:46 +0300

    New project template created by Xcode

---
+++ Questify/Questify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Questify
+//
+//  Created by Coskun Appwox on 16.03.2019.
+//  Copyright © 2019 Coskun Appwox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
